c1339524e9c0be2f5915496c3eda76bdcbb8d765
LostInTheSea <gabrielu@viken.no>
2023-08-29 12:37:05 +0200

    Oppdaterte README.md

--- react_intro/react_intro/src/myButton.js
+++ react_intro/react_intro/src/myButton.js
@@ -1,15 +1,40 @@
 import './App.css';
 
-function HelloWorld() {
-    console.log('wha');
+function clickButton() {
+  console.log('Button clicked');
 }
+
+function Uselist() {
+
+  const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+  ];
+
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+    }}
+  >
+    {product.title}
+  </li>
+  );
+
+  return <ul>{listItems}</ul>
+  
+}  
 
 export default function MyButton() {
 
+  
     return (
       <div>
         <h1>Welcome to my app</h1>
-        <button className='button' onClick={HelloWorld}>I'm a button</button>
+        <button className='button' onClick={clickButton}>I'm a button</button>
+        <Uselist />
       </div>
     );
   }
