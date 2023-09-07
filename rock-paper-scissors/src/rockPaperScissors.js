6291109aad8f76da0c8c6354bf338c81111490e9
The-Gaia-Project <gabrielueland42@yahoo.com>
2023-09-07 09:54:18 +0200

    fixed my user so now i will be sending it with the right one :)

---
+++ rock-paper-scissors/src/rockPaperScissors.js
@@ -0,0 +1,15 @@
+function R(){
+
+    return(
+        <h1>Rock Paper Scissors</h1>
+    );
+}
+export default function rockPaperScissors() {
+    return (
+      <div>
+        <h1>Welcome to Rock Paper Scissors</h1>
+        <R />
+      </div>
+    );
+  }
+  
