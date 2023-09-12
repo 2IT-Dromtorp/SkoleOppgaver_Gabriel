b0c01d7599f9dc561cdf096e25965e7e485c85dc
The-Gaia-Project <gabrielueland42@yahoo.com>
2023-09-12 12:47:50 +0200

    counter is donegit add .!

--- counter/src/Counter.js
+++ counter/src/Counter.js
@@ -5,23 +5,22 @@ export default function Counter() {
     let [letup, setLetup] = useState (0)
     
     let upPress =() => {
-      setLetup(letup +1);
+      setLetup(letup + 1);
     };
 
     let midPress =() => {
-      setLetup(letup == 0);
+      setLetup(letup = 0);
     };
 
     let downPress =() => {
-      setLetup(letup -1);
+      setLetup(letup - 1);
     };
     return (
       <div>
-        <h1>Welcome to my app</h1>
-        <p>{<button onClick={upPress} id="uppre">I'm a up</button>}</p>
-        <p>{<button onClick={midPress} id="midre">I'm a middle</button>}</p>
-        <p>{<button onClick={downPress} id="dowre">I'm a down</button>}</p>
-        <p>{letup}</p>
+        <h1>Welcome to my counter app</h1>
+        <p>{<button onClick={upPress} id="uppre">+</button>}</p>
+        <p>{<button onClick={midPress} id="midre">{letup}</button>}</p>
+        <p>{<button onClick={downPress} id="dowre">-</button>}</p>
       </div>
     );
   }
