5f113fb559511b39b6166cd9c5f8058ea0fe1a20
The-Gaia-Project <gabrielueland42@yahoo.com>
2023-09-13 13:50:05 +0200

    working on rockpaperscissors

--- rock-paper-scissors/src/rockPaperScissors.js
+++ rock-paper-scissors/src/rockPaperScissors.js
@@ -1,15 +1,35 @@
-function R(){
+import {useState} from 'react';
 
-    return(
-        <h1>Rock Paper Scissors</h1>
-    );
-}
-export default function RockPaperScissors() {
-    return (
-      <div>
-        <h1>Welcome to Rock Paper Scissors</h1>
-        <R />
-      </div>
-    );
+function RockPaperScissors () {
+  const [num, setNum] = useState(0);
+  function randomNumberInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  
+
+  function handleClick () {
+    setNum(randomNumberInRange(1, 3));
+  };
+  let R = "Rock";
+  let P = "Paper";
+  let S = "scissors";
+  function Rps () {
+    if (num = 1) {
+      return "R";
+    }
+    else if (num = 2) {
+      return "P";
+    }
+    else if (num = 3) {
+      return "S";
+    }
+  }
+  return (
+    <div>
+      <h2>{Rps}</h2>
+      <h2>number is: {num}</h2>
+      <button onClick={handleClick}>Generate random number</button>
+    </div>
+  );
+};
+
+export default RockPaperScissors;
