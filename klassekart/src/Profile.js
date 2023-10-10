aa6a4fdf36c7800c5e8432f333501f991a1a3c2e
The-Gaia-Project <gabrielueland42@yahoo.com>
2023-10-10 14:21:08 +0200

    klassekart-fix-og-bygget-på-videre

--- klassekart/src/Profile.js
+++ klassekart/src/Profile.js
@@ -1,13 +1,33 @@
 import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import json from './profiles';
 
 export default function Profile() {
 
     const navigate = useNavigate();
-    
+
+    let profileParams = useParams();
+
+    json.elever.map((item, index) => {
+        if (item.navn.toLowerCase() == profileParams.profile.toLowerCase()) {
+            console.log("correct. do something")
+        }
+        else{
+            console.log("Wrong. Dose nothing")
+        }
+    })
+
     return (
         <>
-            <h1> Dette er profilen til eleven </h1>
-            <button onClick={() => navigate(-1)}> Tilbake til hovedmeny </button>
+        <div className='profile-container'>
+
+        <div className='profile-box'>
+
+            <h1> Dette er profilen til {profileParams.profile} </h1>
+            <button onClick={() => navigate(-1)} className='backbuttonlols'> Tilbake til hovedmeny </button>
+
+        </div>
+        </div>
         </>
     )
 }
---
+++ klassekart/src/profiles.js
@@ -0,0 +1,104 @@
+let json = {
+    "elever" : [
+        {
+            "navn" : "Andreas Hurlen Kristiansen",
+            "alder" : "17",
+            "email" : "andreashkr@viken.no",
+            "Tlf" : 90918378,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Ahmad Murtaza Zahid",
+            "alder" : "17",
+            "email" : "ahmadza@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Philip Beyer",
+            "alder" : "17",
+            "email" : "philipbey@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Gabriel Karisari Ueland",
+            "alder" : "23",
+            "email" : "gabrielu@viken.no",
+            "Tlf" : 95006508,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Theodor Julien-Løvås",
+            "alder" : "17",
+            "email" : "theodorju@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Mattis Eintveit Møller",
+            "alder" : "17",
+            "email" : "mattismol@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Alva Cecilia Øvrom",
+            "alder" : "17",
+            "email" : "alvaov@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Silas Surland-Fjær",
+            "alder" : "17",
+            "email" : "silass@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Axel Sequeida Sandbakken",
+            "alder" : "17",
+            "email" : "axelsan@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Vetle Fongen",
+            "alder" : "17",
+            "email" : "vetlefon@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Kristoffer Bekkevold",
+            "alder" : "17",
+            "email" : "kristofbek@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Johannes Herman Rebård Evensen",
+            "alder" : "17",
+            "email" : "johanneeve@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Elias Høgrann Godager",
+            "alder" : "17",
+            "email" : "eliasgod@viken.no",
+            "Tlf" : 999,
+            "image": "images/profile-image.jpg"
+        },
+        {
+            "navn" : "Matheo Kant Pangopoulos",
+            "alder" : "17",
+            "email" : "matheop@viken.no",
+            "Tlf" : 999,
+            "image": "mages/profile-image.jpg"
+        },
+    ]
+}
+
+export default json;
