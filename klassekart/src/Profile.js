6c56ba9cd606ad76299a4be4fb2503eb9d47bb93
The-Gaia-Project <gabrielueland42@yahoo.com>
2023-10-12 09:58:26 +0200

    you-try-and-try-again-only-to-hit-your-head-on-the-same-wall-again-and-again

--- klassekart/src/Profile.js
+++ klassekart/src/Profile.js
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import json from './profiles';
+import { useState } from 'react';
 
 export default function Profile() {
 
@@ -8,12 +9,25 @@ export default function Profile() {
 
     let profileParams = useParams();
 
+    const [profile, setprofile] = useState({
+            "navn" : null,
+            "FullName" : null,
+            "alder" : null,
+            "email" : null,
+            "Tlf" : null,
+            "image": null
+    })
+
+
+
     json.elever.map((item, index) => {
         if (item.navn.toLowerCase() == profileParams.profile.toLowerCase()) {
             console.log("correct. do something")
+            console.log(item)
+
         }
         else{
-            console.log("Wrong. Dose nothing")
+            console.log("Error-Can't-get-the-info-for-elever-form-json")
         }
     })
 
@@ -21,12 +35,15 @@ export default function Profile() {
         <>
         <div className='profile-container'>
 
-        <div className='profile-box'>
+            <div className='profile-box'>
 
-            <h1> Dette er profilen til {profileParams.profile} </h1>
-            <button onClick={() => navigate(-1)} className='backbuttonlols'> Tilbake til hovedmeny </button>
+                <h1> Dette er profilen til {profileParams.profile} </h1>
+                <p>name: {}</p>
 
-        </div>
+
+                <button onClick={() => navigate(-1)} className='backbuttonlols'> Tilbake til hovedmeny </button>
+
+            </div>
         </div>
         </>
     )
--- klassekart/src/profiles.js
+++ klassekart/src/profiles.js
@@ -1,98 +1,112 @@
 let json = {
     "elever" : [
         {
-            "navn" : "Andreas Hurlen Kristiansen",
+            "navn" : "Andreas",
+            "FullName" : "Andreas Hurlen Kristiansen",
             "alder" : "17",
             "email" : "andreashkr@viken.no",
             "Tlf" : 90918378,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Ahmad Murtaza Zahid",
+            "navn" : "Ahmad",
+            "FullName" : "Ahmad Murtaza Zahid",
             "alder" : "17",
             "email" : "ahmadza@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Philip Beyer",
+            "navn" : "Philip",
+            "FullName" : "Philip Beyer",
             "alder" : "17",
             "email" : "philipbey@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Gabriel Karisari Ueland",
+            "navn" : "Gabriel",
+            "FullName" : "Gabriel Karisari Ueland",
             "alder" : "23",
             "email" : "gabrielu@viken.no",
             "Tlf" : 95006508,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Theodor Julien-Løvås",
+            "navn" : "Theodor",
+            "FullName" : "Theodor Julien-Løvås",
             "alder" : "17",
             "email" : "theodorju@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Mattis Eintveit Møller",
+            "navn" : "Mattis",
+            "FullName" : "Mattis Eintveit Møller",
             "alder" : "17",
             "email" : "mattismol@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Alva Cecilia Øvrom",
+            "navn" : "Alva",
+            "FullName" : "Alva Cecilia Øvrom",
             "alder" : "17",
             "email" : "alvaov@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Silas Surland-Fjær",
+            "navn" : "Silas",
+            "FullName" : "Silas Surland-Fjær",
             "alder" : "17",
             "email" : "silass@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Axel Sequeida Sandbakken",
+            "navn" : "Axel",
+            "FullName" : "Axel Sequeida Sandbakken",
             "alder" : "17",
             "email" : "axelsan@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Vetle Fongen",
+            "navn" : "Vetle",
+            "FullName" : "Vetle Fongen",
             "alder" : "17",
             "email" : "vetlefon@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Kristoffer Bekkevold",
+            "navn" : "Kristoffer",
+            "FullName" : "Kristoffer Bekkevold",
             "alder" : "17",
             "email" : "kristofbek@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Johannes Herman Rebård Evensen",
+            "navn" : "Johannes",
+            "FullName" : "Johannes Herman Rebård Evensen",
             "alder" : "17",
             "email" : "johanneeve@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Elias Høgrann Godager",
+            "navn" : "Elias",
+            "FullName" : "Elias Høgrann Godager",
             "alder" : "17",
             "email" : "eliasgod@viken.no",
             "Tlf" : 999,
             "image": "images/profile-image.jpg"
         },
         {
-            "navn" : "Matheo Kant Pangopoulos",
+            "navn" : "Matheo",
+            "FullName" : "Matheo Kant Pangopoulos",
             "alder" : "17",
             "email" : "matheop@viken.no",
             "Tlf" : 999,
