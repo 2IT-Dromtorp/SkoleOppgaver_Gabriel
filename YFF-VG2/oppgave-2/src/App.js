0e6f463be5f88b45e25c993d110810a606811abc
The-Gaia-Project <gabrielueland42@yahoo.com>
2023-11-16 10:17:30 +0100

    YFF-Oppgaver

---
+++ YFF-VG2/oppgave-2/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <div className="headerimage">
+         <img src={logo} alt="logo" />
+        </div>{/*headerimage*/}
+        <div className="headertext">
+          <h1>YFF Oppgave 2. Quiz</h1>
+        </div>{/*headertext*/}
+      </header>
+      <div>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
