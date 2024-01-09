797b999ff87bb994ded1d3ce4d61b7cdc8beedb8
The-Gaia-Project <gabrielueland42@yahoo.com>
2024-01-09 14:01:01 +0100

    commit

--- nodeApplications/helloWorld/index.js
+++ nodeApplications/helloWorld/index.js
@@ -34,6 +34,8 @@ app.get('/', (request, response) => {
 
 
 app.get("/updateuser/:newhobby/:id", (request, response) => {
+
+  //http://localhost:3000/updateuser/walking/12
   
   let newhobby = request.params.newhobby;
   let id = request.params.id;
--- nodeApplications/httprequests/src/update.js
+++ nodeApplications/httprequests/src/update.js
@@ -2,66 +2,28 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function UpdateUserForm() {
-    const [users, setUsers] = useState([]); 
-    const [newHobby, setNewHobby] = useState('');
-    const [selectedUserId, setSelectedUserId] = useState('');
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get("http://localhost:3000/");
-                setUsers(response.data);
-                if (response.data.length > 0) {
-                    setSelectedUserId(response.data[0].ElevID);
-                }
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    const handleSubmit = (event) => {
+    function handleSubmit(event) {
+        const [updateElevData, setupdateElevData] = useState([])
         event.preventDefault();
-        const url = `http://localhost:3000/updateuser/${encodeURIComponent(newHobby)}/${encodeURIComponent(selectedUserId)}`;
-
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                alert('User updated successfully!');
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('There was an error updating the user!', error);
-            });
-    };
+        console.log('sybmitted');
+        const updateElevDatarequest = () => {
+            azios
+                .get('http:')
+                .then(response => {
+                    setupdateElevData(response.date);
+                })
+        }
+    }
 
     return (
-        <div>
-            <h2>Update User Hobby</h2>
-            <form onSubmit={handleSubmit}>
-            <label>
-                    New Hobby:
-                    <input type="text" value={newHobby} onChange={(e) => setNewHobby(e.target.value)}/>
-            </label>
-                <br />
-                <label>
-                    User ID:
-                    <select 
-                        value={selectedUserId} 
-                        onChange={(e) => setSelectedUserId(e.target.value)}
-                    >
-                        {users.map(user => (
-                            <option key={user.ElevID} value={user.ElevID}>
-                                {user.Fornavn} {user.Etternavn} (ID: {user.ElevID})
-                            </option>
-                        ))}
-                    </select>
-                </label>
-                <br />
-                <button type="submit">Update</button>
-            </form>
-        </div>
+        <>
+        <form onSubmit={handleSubmit}>
+            <label>ny Hobby:</label>
+            <input type='text' placeholder='john'/>
+            <label>ElevID:</label>
+            <input type='number'/>
+            <input type='submit' value={"Submit"}/>
+        </form>
+        </>
     );
 }
