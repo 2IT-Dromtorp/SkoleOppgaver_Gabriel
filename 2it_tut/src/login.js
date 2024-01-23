afdb4e37e340ec0f8560acd94b1fd0f8f7bd1e2a
The-Gaia-Project <gabrielueland42@yahoo.com>
2024-01-23 14:22:49 +0100

    commit

--- 2it_tut/src/login.js
+++ 2it_tut/src/login.js
@@ -1,32 +1,36 @@
 import "./login.css";
 import lake from "./LackImage.png";
-export default function Login () {
+
+export default function Login() {
+        
+
+    const handleLogin = () => {
+        console.log('Login button clicked');
+    };
+
     const backgroundImageStyle = {
         backgroundImage: `url(${lake})`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         width: '100%',
-        height: 'auto', // You can adjust the height as needed
-      };    
-    return(
+        height: '100vh',
+    };
+
+    return (
         <>
             <div style={backgroundImageStyle} id="Background">
                 <div id="LoginBox">
                     <h2>Logg inn</h2>
                     <p>Brukernavn</p>
-                    <label for="username"></label>
+                    <label htmlFor="username"></label>
                     <input type="text" id="username" name="username"></input>
                     
                     <p>Passord</p>
-                    <label for="username"></label>
-                    <input type="text" id="username" name="username"></input>
-                    
-                    <p>Bekreft Passord</p>
-                    <label for="username"></label>
-                    <input type="text" id="username" name="username"></input>
+                    <label htmlFor="password"></label>
+                    <input type="password" id="password" name="password"></input><br/>
+                    <button onClick={handleLogin} id="buttonLogin">Logg inn</button>
                 </div>
             </div>
-            
         </>
     );
 }
